b07f3485c646ece37091229f944bc8d7bbf6c284
Michalenok Marina <mihalenokee@outlook.com>
2024-06-29 12:04:22 +0300

    add RateService

--- src/main/java/com/michalenok/currency_rate_guide/controller/CurrencyRateController.java
+++ src/main/java/com/michalenok/currency_rate_guide/controller/CurrencyRateController.java
@@ -1,5 +1,7 @@
 package com.michalenok.currency_rate_guide.controller;
 
+import com.michalenok.currency_rate_guide.service.RateService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,10 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class CurrencyRateController {
+    private RateService rateService;
     @PostMapping("/rates")
     public void saveRates(@RequestParam(name = "periodicity", required = false, defaultValue = "0") String periodicity,
                           @RequestParam(required = false) String ondate) {
-
+        rateService.saveRates(periodicity, ondate);
     }
 }
---
+++ src/main/java/com/michalenok/currency_rate_guide/service/RateService.java
@@ -0,0 +1,6 @@
+package com.michalenok.currency_rate_guide.service;
+
+public interface RateService {
+    void saveRates(String periodicity, String ondate);
+
+}
