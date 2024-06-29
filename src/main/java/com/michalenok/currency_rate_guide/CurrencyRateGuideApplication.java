d76cd6353bc9b289f556e1ca92b3a6737b9e0205
Michalenok Marina <mihalenokee@outlook.com>
2024-06-29 11:20:01 +0300

    initial commit

---
+++ src/main/java/com/michalenok/currency_rate_guide/CurrencyRateGuideApplication.java
@@ -0,0 +1,13 @@
+package com.michalenok.currency_rate_guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyRateGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyRateGuideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michalenok/currency_rate_guide/CurrencyRateGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michalenok.currency_rate_guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyRateGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
