2d74e90bbe3bc26aee6e06c58abe9f775226bb68
Michalenok Marina <mihalenokee@outlook.com>
2024-06-30 01:43:44 +0300

    add ExceptionGlobal, ExceptionErrorDTO

---
+++ src/main/java/com/michalenok/currency_rate_guide/web/ExceptionGlobal.java
@@ -0,0 +1,33 @@
+package com.michalenok.currency_rate_guide.web;
+
+import com.michalenok.currency_rate_guide.model.error.ExceptionErrorDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class ExceptionGlobal {
+    /**
+     * 404
+     */
+    @ExceptionHandler(value = {NoSuchElementException.class })
+    public ResponseEntity<List<ExceptionErrorDTO>> ArgumentNotFoundException(
+            RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(List.of(new ExceptionErrorDTO(e.getMessage())));
+    }
+
+    /**
+     * 500
+     */
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public List<ExceptionErrorDTO> handler(Throwable e) {
+        return List.of(new ExceptionErrorDTO(e.getMessage()));
+    }
+}
