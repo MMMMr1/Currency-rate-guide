ca5e14be5cc09eda03cdd0c3eedbec589b247d97
Michalenok Marina <mihalenokee@outlook.com>
2024-06-29 11:48:02 +0300

    add NbrbClient, RateResponse, dependency for FeinClient

---
+++ src/main/java/com/michalenok/currency_rate_guide/client/NbrbClient.java
@@ -0,0 +1,17 @@
+package com.michalenok.currency_rate_guide.client;
+
+import com.michalenok.currency_rate_guide.model.dto.RateResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(name = "nbrbClient", url = "${nbrb.url}")
+public interface NbrbClient {
+    @GetMapping(path = "/exrates/rates?{periodicity}&{ondate}")
+    List<RateResponse> getRates(
+            @RequestParam String periodicity,
+            @RequestParam String ondate
+    );
+}
