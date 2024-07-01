ee33bc891102b2210281695709fce27ea6fc43fc
Michalenok Marina <mihalenokee@outlook.com>
2024-07-01 10:06:22 +0300

    add exception handlers for MethodArgumentNotValidException and ConstraintViolationException

--- src/main/java/com/michalenok/currency_rate_guide/web/ExceptionGlobalHandler.java
+++ src/main/java/com/michalenok/currency_rate_guide/web/ExceptionGlobalHandler.java
@@ -1,17 +1,44 @@
 package com.michalenok.currency_rate_guide.web;
 
 import com.michalenok.currency_rate_guide.model.error.ExceptionErrorDTO;
+import com.michalenok.currency_rate_guide.model.error.ExceptionListDTO;
+import com.michalenok.currency_rate_guide.model.error.ExceptionStructuredDTO;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ExceptionGlobalHandler {
+    /**
+     * 400
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionListDTO> onMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        List<ExceptionStructuredDTO> error = e.getBindingResult().getFieldErrors().stream()
+                .map(s -> new ExceptionStructuredDTO(s.getField(), s.getDefaultMessage()))
+                .collect(Collectors.toList());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ExceptionListDTO(error));
+    }
+    /**
+     * 400
+     */
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List<ExceptionErrorDTO>> onConstraintViolationException(
+            ConstraintViolationException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(List.of(new ExceptionErrorDTO(e.getMessage())));
+    }
+
     /**
      * 404
      */
